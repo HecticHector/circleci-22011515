dfb7aa2b364cbb5b1bd70806b52f3147c2aa02db
apple <apple@apples-MacBook-Pro.local>
2022-01-15 19:43:49 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+circleci-22011515
+
+/* jest */ [
+  https://jestjs.io/docs/getting-started
+
+  npm install --save-dev jest
+  npm test
+]
+
---
+++ operations.test.js
@@ -0,0 +1,15 @@
+const addSum = require('./operations');
+
+// ES6 arrow form
+test('adds 1 + 2 to equal 3', () => {
+  expect(addSum(1, 2)).toBe(3);
+});
+
+// same as previous decoupled with function outside
+test('adds 2 + 3 to equal 5', addSumTest);
+
+function addSumTest() {
+  expect(addSum(2, 3)).toBe(5);
+}
+
+
